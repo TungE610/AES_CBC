be7986e080e333b2c62f08df9b9656b682a773c0
TungE610 <81414026+TungE610@users.noreply.github.com>
2021-12-24 22:51:00 +0700

    Add files via upload

---
+++ AES_128_CBC_mode.py
@@ -0,0 +1,45 @@
+import secrets
+from base64 import b64encode, b64decode
+from binascii import unhexlify
+
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+# Sinh ngau nhien IV
+iv = secrets.token_hex(16)
+key = "3e83b13d99bf0de6c6bde5ac5ca4ae68"
+pText = "mynameisbuidanhtungdeptraivcl"
+
+print(f"IV: {iv}")
+print(f"Key: {key}")
+print(f"Plain Text: {pText}")
+
+# chuyen chuoi hex ve dang binary
+iv = unhexlify(iv)
+key = unhexlify(key)
+
+# Encode và Padding 
+print("pText-encode : ",pText.encode())
+pText = pad(pText.encode(), AES.block_size)
+print("after-padding : ",pText)
+
+# Encipher chuoi
+cipher = AES.new(key, AES.MODE_CBC, iv)
+cipher_text = cipher.encrypt(pText)
+
+
+# Encode Cipher_text as Base 64 and decode to String
+out = b64encode(cipher_text).decode('utf-8')
+print(f"Cipher Text: {out}")
+
+
+#####DECRYPTION##########
+
+# Decipher ban ma
+decipher = AES.new(key, AES.MODE_CBC, iv)
+plaintext = decipher.decrypt(b64decode(out))
+
+# UnPadding
+plaintext = unpad(plaintext, AES.block_size).decode('utf-8')
+print(f'Plain Text: {plaintext}')
+
